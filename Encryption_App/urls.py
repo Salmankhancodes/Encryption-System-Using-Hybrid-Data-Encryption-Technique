ea07ca42cc3d5e37e8f091ec987b43ed23671c9e
Salmankhancodes <117salmankhan@gmail.com>
2020-11-27 21:26:16 +0530

    Initial Django Setup

--- Encryption_App/urls.py
+++ Encryption_App/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from Encryption_App import views
+
 urlpatterns = [
     path("",views.index, name="index"),
+    path("encryption",views.encryptionalgo, name="encryption"),
+    path("decryption",views.decryptionalgo, name="decryption"),
 ]
--- Encryption_App/views.py
+++ Encryption_App/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Hello world")
+    context={'Encrypteddata':data}
+    return render(request,"index.html",context)
+
+def encryptionalgo(request):
+    # return render(request,"index.html")
+    return HttpResponse("This is encryption function test")
+
+
+
+def decryptionalgo(request):
+    # return render(request,"index.html")
+    return HttpResponse("This is decryption function test")
