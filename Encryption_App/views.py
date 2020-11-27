5137d8f0ca9f83ef31c80906bd845c120e29788c
Salmankhancodes <117salmankhan@gmail.com>
2020-11-27 21:31:02 +0530

    Added fibonacci sequence Method for data encryption.

--- Encryption_App/views.py
+++ Encryption_App/views.py
@@ -2,6 +2,66 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+def fibogen ( lenofpassword ) :
+    n = int ( lenofpassword )
+    first = int ( 1 )
+    next = int ( 1 )
+    list = [ ]
+    list.append ( first )
+    list.append ( next )
+
+    current = first + next
+    for i in range ( lenofpassword ) :
+        list.append ( current )
+        first = next
+        next = current
+        current = first + next
+
+    return list
+
+
+def split_string ( string ) :
+    li = [ ]
+    l = len ( string )
+    dif = l // 3
+    li.append ( string [ :dif ] )
+    li.append ( string [ dif :2 * dif ] )
+    li.append ( string [ 2 * dif : ] )
+    print ( li )
+    return li
+
+
+def fiboEncryption ( data , password ) :
+    reversedtext = data [ : :-1 ]
+
+    lenofpassword = len ( password )
+
+    ciphertext = reversedtext + password
+
+    rs = fibogen ( lenofpassword )
+
+    for i in rs :
+        new = ""
+        for ch in range ( len ( ciphertext ) ) :
+
+            if ch % 2 != 0 :
+                x = chr ( ord ( ciphertext [ ch ] ) + i )
+
+                new += x
+            else :
+                x = ord ( ciphertext [ ch ] ) - i
+                if x < 0 :
+                    x = abs ( 110000 + x )
+                x = chr ( x )
+
+                new += x
+
+        ciphertext = new
+
+    return ciphertext
+
+
 def index(request):
     context={'Encrypteddata':data}
     return render(request,"index.html",context)
